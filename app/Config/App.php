cd818da993b7af224a16e0604ac3997c8da512ca
Karan Bhaiya <karan@example.com>
2026-09-17 18:36:31 +0530

    Fix reverse proxy infinite redirect loop

--- app/Config/App.php
+++ app/Config/App.php
@@ -342,7 +342,7 @@ class App extends BaseConfig
 	 *
 	 * @var string|string[]
 	 */
-	public $proxyIPs = '';
+	public $proxyIPs = '*';
 
 	/**
 	 * --------------------------------------------------------------------------
